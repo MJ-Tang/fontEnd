7875457ba255646a4b228f1fbcca86c35da02ab4
MJ-Tang <33923014+MJ-Tang@users.noreply.github.com>
2022-03-03 21:50:03 +0800

    First React Project

---
+++ React/fitstProject/src/App.js
@@ -0,0 +1,17 @@
+
+// import './App.css';
+import './style.css'
+import React from 'react';
+import NavBar from './componets/Navbar';
+import Main from './componets/Main';
+
+function App() {
+  return (
+      <div className='container'>
+        <NavBar />
+        <Main />
+      </div>
+  );
+}
+
+export default App;
