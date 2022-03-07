9ddd1432cc4fffada98abd71b6ad86916d19acd3
MJ-Tang <33923014+MJ-Tang@users.noreply.github.com>
2022-03-07 09:58:03 +0800

    use props to show the data

--- React/airbnb-clone/src/components/Cards.js
+++ React/airbnb-clone/src/components/Cards.js
@@ -1,19 +1,19 @@
 import React from "react";
-import katie from '../images/katie-zaferes.png'
 import star from '../images/star.png'
 
-export default function Card() {
+export default function Card(props) {
+    console.log(props);
     return(
         <div className="card">
-            <img src={katie} alt="" className="card-image" />
+            <img src={require(`../images/${props.img}`)} alt="" className="card-image" />
             <div className="card-starts">
                 <img src={star} alt="" />
-                <span>5.0</span>
-                <span className="gray">(6) •</span>
-                <span className="gray">USA</span>
+                <span>{props.rating}</span>
+                <span className="gray">({props.reviewCount}) •</span>
+                <span className="gray">{props.country}</span>
             </div>
-            <p>Life Lessons with Katie Zaferes</p>
-            <p> <span className="blod"> From $136</span> / person</p>
+            <p>{props.title}</p>
+            <p> <span className="blod"> From ${props.price}</span> / person</p>
         </div>
     )
 }
