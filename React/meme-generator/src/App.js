a0e7b5a0989fff71279a082cfe698935af78461b
MJ-Tang <33923014+MJ-Tang@users.noreply.github.com>
2022-03-12 21:34:54 +0800

    create a func to get img url from data.js

---
+++ React/meme-generator/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import './style.css';
+import React from 'react';
+import Header from './components/Header';
+import Meme from './components/Meme';
+
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Meme />
+    </div>
+  );
+}
+
+export default App;
