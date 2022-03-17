2fc62eedc3f39aae17e3a5fbd9ce1b1e669b8fb5
MJ-Tang <33923014+MJ-Tang@users.noreply.github.com>
2022-03-17 22:04:23 +0800

    show top and bottom text on image

--- React/meme-generator/src/components/Meme.js
+++ React/meme-generator/src/components/Meme.js
@@ -3,15 +3,36 @@ import memesData from "../memesData";
 
 export default function Meme() {
 
-    const [memeImage, setMemeImage] = React.useState("")
+    // const [memeImage, setMemeImage] = React.useState("")
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg"
+    })
+
+    const [allMemeImages, setMemeImages] = React.useState(memesData)
     
 
     function getMemeImage() {
-        const memesArr = memesData.data.memes
+        const memesArr = allMemeImages.data.memes
         const randomN = Math.floor(Math.random() * memesArr.length)
-        setMemeImage(memesArr[randomN].url)
-        console.log(memeImage);
+        const url = memesArr[randomN].url
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: url
+        }))
+        console.log(meme);
     }
+
+    function handleChange(e) {
+        const {name, value} = e.target
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name]: value
+        }))
+    }
+    console.log(meme);
+
 
     return (
         <main>
@@ -20,12 +41,18 @@ export default function Meme() {
                     type="text"
                     placeholder="Top text"
                     className="form-input"
+                    name="topText"
+                    value={meme.topText}
+                    onChange={handleChange}
                 />
 
                 <input 
                     type="text"
                     placeholder="Bottom text"
                     className="form-input"
+                    name="bottomText"
+                    value={meme.bottomText}
+                    onChange={handleChange}
                 />
 
                 <button 
@@ -35,7 +62,11 @@ export default function Meme() {
                     Get a new meme image
                 </button>
             </div>
-            <img src={memeImage} alt="" srcset="" className="meme-image" />
+            <div className="meme">
+                <img src={meme.randomImage} alt="" srcset="" className="meme-image" />
+                <h2 className="meme-text top">{meme.topText}</h2>
+                <h2 className="meme-text bottom">{meme.bottomText}</h2>
+            </div>
         </main>
     )
 }
