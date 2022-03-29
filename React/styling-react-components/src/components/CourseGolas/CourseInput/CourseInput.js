c9ab71c5cc25e4030a35ad89c43856f68c27ad3c
MJ-Tang <33923014+MJ-Tang@users.noreply.github.com>
2022-03-29 22:03:12 +0800

    use styled in course-input.js

--- React/styling-react-components/src/components/CourseGolas/CourseInput/CourseInput.js
+++ React/styling-react-components/src/components/CourseGolas/CourseInput/CourseInput.js
@@ -1,6 +1,35 @@
 import React, {useState} from "react";
 import Button from "../../UI/Button/Button";
 import './CourseInput.css';
+import styled from "styled-components";
+
+const FormControl = styled.div`
+    margin: 0.5rem 0;
+
+    & label {
+        font-weight: bold;
+        display: block;
+        margin-bottom: 0.5rem;
+        color: ${props => (props.invalid ? 'red' : 'black')};
+    }
+
+    & input {
+        display: block;
+        width: 100%;
+        border: 1px solid ${props => (props.invalid ? 'red' : '#ccc')};
+        background: ${props => (props.invalid ? '#ffd7d7' : 'transparent')};
+        font: inherit;
+        line-height: 1.5rem;
+        padding: 0 0.25rem;
+    }
+
+    & input:focus {
+        outline: none;
+        background-color: #fad0ec;
+        border-color: #8b005d;
+    }
+`
+
 
 const CourseInput = props => {
 
@@ -27,7 +56,7 @@ const CourseInput = props => {
     
     return ( 
         <form onSubmit={formSumbitHandler}>
-            <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
+            <FormControl invalid={!isValid}>
                 <label>
                     Course Goal
                 </label>
@@ -35,7 +64,7 @@ const CourseInput = props => {
                     type="text" 
                     onChange={goalInputChangeHandler} 
                 />
-            </div>
+            </FormControl>
 
             <Button type="submit">Add Goal</Button>
         </form>
