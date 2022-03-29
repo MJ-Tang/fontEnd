4a27b31346b04eb551ddd89b0f5c7f0e3128fc85
MJ-Tang <33923014+MJ-Tang@users.noreply.github.com>
2022-03-29 15:33:24 +0800

    chick is input value empty

--- React/styling-react-components/src/components/CourseGolas/CourseInput/CourseInput.js
+++ React/styling-react-components/src/components/CourseGolas/CourseInput/CourseInput.js
@@ -5,13 +5,21 @@ import './CourseInput.css';
 const CourseInput = props => {
 
     const [enteredValue, setenteredValue] = useState('');
+    const [isValid, setisValid] = useState(true);
 
     const goalInputChangeHandler = e => {
+        if (e.target.value.trim().length > 0) {
+            setisValid(true)
+        }
         setenteredValue(e.target.value)
     }
 
     const formSumbitHandler = e => {
         e.preventDefault()
+        if(enteredValue.trim().length === 0) {
+            setisValid(false)
+            return
+        }
         props.onAddGoal(enteredValue)
     }
 
@@ -20,8 +28,19 @@ const CourseInput = props => {
     return ( 
         <form onSubmit={formSumbitHandler}>
             <div className="form-control">
-                <label>Course Goal</label>
-                <input type="text" onChange={goalInputChangeHandler} />
+                <label
+                    style={{ color: !isValid ? 'red' : 'black' }}
+                >
+                    Course Goal
+                </label>
+                <input 
+                    type="text" 
+                    onChange={goalInputChangeHandler} 
+                    style={{ 
+                        borderColor: !isValid ? 'red' : 'black', 
+                        backgroud: !isValid ? 'salmon' : 'transparent'
+                    }}
+                />
             </div>
 
             <Button type="submit">Add Goal</Button>
